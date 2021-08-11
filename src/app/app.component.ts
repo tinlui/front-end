1091d1b1a48b867243607c74e038834c29a144f9
tinlui <larobledog@gmail.com>
2021-08-11 15:52:25 -0500

    Nuevo componente y uso del ngIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
-
-  peliculas=[{
-    titulo:'Spider-Man',
-    fechaLanzamiento: new Date(),
-    precio : 1400.99
-  },{
-    titulo:'Moana',
-    fechaLanzamiento: new Date('2016-11-14'),
-    precio : 300.99
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.peliculas=[{
+      titulo:'Spider-Man',
+      fechaLanzamiento: new Date(),
+      precio : 1400.99
+    },{
+      titulo:'Moana',
+      fechaLanzamiento: new Date('2016-11-14'),
+      precio : 300.99
+    }
+  ]
   }
-]
+  title = 'front-end';
+ peliculas:any;
 }
