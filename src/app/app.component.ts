ace0bcf4cab4b066ba2996e2491ebcb4073ba7e2
tinlui <larobledog@gmail.com>
2021-08-12 11:37:18 -0500

    Implementacion de Input en Componente listado peliculas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   ngOnInit(): void {
-    this.peliculas=[{
-      titulo:'Spider-Man',
-      fechaLanzamiento: new Date(),
-      precio : 1400.99
-    },{
-      titulo:'Moana',
-      fechaLanzamiento: new Date('2016-11-14'),
-      precio : 300.99
-    }
-  ]
+    this.peliculasEnCines = [
+      {
+        titulo: 'Spider-Man Far From Home',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99,
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99,
+      },
+    ];
   }
   title = 'front-end';
- peliculas:any;
+  peliculasEnCines: any;
+
+  peliculasProximosEstrenos = [
+    {
+      titulo: 'Avengers',
+      fechaLanzamiento: new Date(),
+      precio: 1400.99,
+    },
+    {
+      titulo: 'Inception',
+      fechaLanzamiento: new Date('2016-11-14'),
+      precio: 300.99,
+    },
+    {
+      titulo: 'Rocky',
+      fechaLanzamiento: new Date('2016-11-14'),
+      precio: 300.99,
+    },
+  ];
 }
--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-peliculas',
@@ -9,7 +9,11 @@ export class ListadoPeliculasComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  peliculas:any;
+
   ngOnInit(): void {
+   
   }
 
 }
