42ddb5501c7e9d52154d564358ed4359ee3155bc
tinlui <larobledog@gmail.com>
2021-09-07 13:49:24 -0500

    editar  genero

--- src/app/generos/editar-genero/editar-genero.component.ts
+++ src/app/generos/editar-genero/editar-genero.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { generoCreacionDTO } from '../genero';
+import { ActivatedRoute, Router } from '@angular/router';
+import { parsearErroresAPI } from 'src/app/utilidades/utilidades';
+import { generoCreacionDTO, generoDTO } from '../genero';
+import { GenerosService } from '../generos.service';
 
 @Component({
   selector: 'app-editar-genero',
   templateUrl: './editar-genero.component.html',
-  styleUrls: ['./editar-genero.component.css']
+  styleUrls: ['./editar-genero.component.css'],
 })
 export class EditarGeneroComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private generosServices: GenerosService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  constructor(private router: Router) { }
-
-modelo: generoCreacionDTO={nombre:'Drama'};
+  modelo: generoDTO;
+  errores: string[]=[];
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params)=>{
+      this.generosServices.obtenerPorId(params.id)
+      .subscribe(genero=>{
+        this.modelo=genero;
+      }, ()=> this.router.navigate(['/generos']))
+    });
+    
   }
-  guardarCambios(genero:generoCreacionDTO){
-    console.log(genero)
-    this.router.navigate(['/generos'])
+  guardarCambios(genero: generoCreacionDTO) {
+    this.generosServices.editar(this.modelo.id,genero)
+    .subscribe(()=>{
+      this.router.navigate(['/generos']);
+    }, error=>this.errores=parsearErroresAPI(error))
+    
   }
 }
